0b22a7c22dc8e3c81fd3e11d2d5e7a10a59f3911
Hammad Ali <hammadali78056@gmail.com>
2024-08-17 08:22:02 +0500

    error clear

--- basic_react/src/App.jsx
+++ basic_react/src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css'
-import Products from './Products.jsx';
 import Tittle from './Tittle.jsx';
+import Item from './Item.jsx';
 import React from 'react'
 function App() {
-   // array as props
-   let option = ["Hi i am Hammad", "I am a developer", "I am a student"];
-   let option2 = {a:"Hammad", b:"Ali", c:"Awan"};
 return (
   <div>
-<Products title="Mobile" price={30000} features={option} features2={option2}/>
-<Products title="Laptop" price={200000} />
-<Products title="Pen" price={10}/>
+<Item/>
  </div>
 );
 }
